1439b2ad4b57e7496898fbed440caee3034bd2e3
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-20 13:11:16 +0300

    Mission Passed

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,23 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+    console.log(`before setState: ${count}`);
+
     setCount(count + 1);
+
+    console.log(`after setState: ${count}`);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <div onClick={increment}>{count}</div>;
 }
+
+
+//   function increment() {
+//     setCount(count + 1);
+//     setCount(count + 1)
+//   }
+
+//   return <button onClick={increment}>I have been clicked {count} times</button>;
+// }
 
 export default Counter;
