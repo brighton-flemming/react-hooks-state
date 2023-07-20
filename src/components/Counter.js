97ea85ba47d8c64a59531e5a11afb7a335a07f0e
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-20 13:12:47 +0300

    Mission Passed

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,24 +3,14 @@ import React, { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
 
+
+
   function increment() {
-    console.log(`before setState: ${count}`);
-
     setCount(count + 1);
-
-    console.log(`after setState: ${count}`);
+    setCount(count + 1)
   }
 
-  return <div onClick={increment}>{count}</div>;
+  return <button onClick={increment}>I have been clicked {count} times</button>;
 }
-
-
-//   function increment() {
-//     setCount(count + 1);
-//     setCount(count + 1)
-//   }
-
-//   return <button onClick={increment}>I have been clicked {count} times</button>;
-// }
 
 export default Counter;
